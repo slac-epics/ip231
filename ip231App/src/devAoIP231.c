fe15d70435380c493c7c27b5019e71b3e833a761
Kukhee Kim <khkim@slac.stanford.edu>
2019-11-14 18:02:26 -0800

    bug fix EPICS_VERSION check

--- ip231App/src/devAoIP231.c
+++ ip231App/src/devAoIP231.c
@@ -206,7 +206,7 @@ struct IP231_DEV_SUP_SET
     DEVSUPFUN       special_linconv;
 } devAoIP231 = {6, NULL, NULL, init_ao, NULL, write_ao, ao_lincvt};
 
-#if EPICS_VERSION>=3 && EPICS_REVISION>=14
+#if (EPICS_VERSION>=3 && EPICS_REVISION>=14) || EPICS_VERSION>3
 epicsExportAddress(dset, devAoIP231);
 #endif
 
